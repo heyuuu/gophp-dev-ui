6f92735bfcfc5ebab4a06b21bbbcc70d47e952e7
HeYu <code@qheyu.com>
2024-03-10 22:54:24 +0800

    Api: 调整服务端路由，静态资源支持单页面应用；api 统一增加 api/ 路由前缀

--- router/router.go
+++ router/router.go
@@ -3,16 +3,36 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"gophp-dev-ui/controller"
+	"os"
+	"path/filepath"
 )
 
 func InitRouter(r *gin.Engine) {
 	// static
-	r.Static("/static/", "static/")
+	//r.Static("/static/asserts", "static/asserts")
+	r.GET("/static/*filepath", func(c *gin.Context) {
+		path := filepath.Join("static", c.Param("filepath"))
+		if !isFileExist(path) {
+			path = "static/index.html"
+		}
+		c.File(path)
+	})
 	r.GET("/", controller.Index)
 
 	// api
-	r.GET("/test/path_list", controller.ApiHandler(controller.TestPathList))
-	r.GET("/test/list", controller.ApiHandler(controller.TestList))
-	r.GET("/test/detail", controller.ApiHandler(controller.TestDetail))
-	r.POST("/test/run_custom", controller.ApiHandler(controller.TestRunCustom))
+	apiGroup := r.Group("api")
+	{
+		apiGroup.GET("/test/path_list", controller.ApiHandler(controller.TestPathList))
+		apiGroup.GET("/test/list", controller.ApiHandler(controller.TestList))
+		apiGroup.GET("/test/detail", controller.ApiHandler(controller.TestDetail))
+		apiGroup.POST("/test/run_custom", controller.ApiHandler(controller.TestRunCustom))
+	}
+}
+
+func isFileExist(path string) bool {
+	if path == "" {
+		return false
+	}
+	_, err := os.Stat(path)
+	return err == nil
 }
