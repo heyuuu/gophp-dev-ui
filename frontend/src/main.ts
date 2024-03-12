29b36e0ba71822ae3da75864cbafa8f838368d98
HeYu <code@qheyu.com>
2024-03-12 22:37:07 +0800

    FE: fix monaco-editor 无法启动 worker 的问题

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './fixMonaco'
 
 const app = createApp(App)
 
