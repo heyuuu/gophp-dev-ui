d8a706c78fa9e8fb0629671e2457af5aab6463ec
HeYu <code@qheyu.com>
2024-03-12 22:56:47 +0800

    支持指定工作目录；fix 修复 air 下工作目录不正确的问题

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
 dev:
 	cd .. && air -c gophp-dev-ui/all.air.toml
+
+fe-build:
+	cd frontend && npm run build
+
+api-build:
+	go build -o tmp/main
+
+build: fe-build api-build
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gophp-dev-ui/router"
+	"log"
+	"os"
 )
 
 func main() {
+	var workdir string
+	flag.StringVar(&workdir, "w", "", "workdir")
+	flag.Parse()
+	fmt.Println("\n>>> workdir=" + workdir)
+	if workdir != "" {
+		err := os.Chdir(workdir)
+		if err != nil {
+			log.Panicln(err)
+		}
+	}
+
 	r := gin.Default()
 	router.InitRouter(r)
 	r.Run(":8080")
